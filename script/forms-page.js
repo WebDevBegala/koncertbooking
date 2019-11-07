862372e3840acad5ff0cf2303207c8368ee37e66
WebDevBegala <begala.g1999@gmail.com>
2019-11-08 00:28:17 +0100

    Felület csere

---
+++ script/forms-page.js
@@ -0,0 +1,190 @@
+let page1 = ` <div class="input-area">
+<div class="input-title">
+    <p>Keresztnév</p>
+</div>
+<div class="input-block">
+    <input onfocus="focused(event)" id="keresztnev" onblur="unfocused(event)" type="text" class="input"
+        placeholder="Keresztnév" onfocus="focused(event)" onblur="unfocused(event)">
+</div>
+</div>
+<div class="input-area">
+<div class="input-title">
+    <p>Vezetéknév</p>
+</div>
+<div class="input-block">
+    <input onfocus="focused(event)" id="vezeteknev" onblur="unfocused(event)" type="text" class="input"
+        placeholder="Vezetéknév">
+</div>
+</div>
+<div class="input-area">
+<div class="input- le">
+    <p>Születési dátum</p>
+</div>
+<div class="input-block">
+    <input onfocus="focused(event)" onblur="unfocused(event)" type="date" class="input"
+        placeholder="Születési dátum" max="2010-01-01">
+</div>
+</div>
+<div class="input-area">
+<div class="input-title">
+    <p>Jelszó</p>
+</div>
+<div class="input-block">
+    <input onfocus="focused(event)" onblur="unfocused(event)" type="password" class="input"
+        id="password" placeholder="Jelszó">
+</div>
+</div>
+<div class="input-area">
+<div class="input-title">
+    <p>Jelszó mégegyszer</p>
+</div>
+<div class="input-block">
+    <input onfocus="focused(event)" onblur="unfocused(event)" type="password" class="input"
+        id="password2" placeholder="Jelszó mégegyszer">
+</div>
+</div>`;
+
+let page2 = `
+<div class="input-area">
+<div class="input-title">
+    <p>Lakcím</p>
+</div>
+<div class="input-block">
+    <input onfocus="focused(event)" onblur="unfocused(event)" type="text" class="input"
+        placeholder="Lakcím">
+</div>
+</div>
+<div class="input-area">
+<div class="input-title">
+    <p>Telefonszám</p>
+</div>
+<div class="input-block">
+    <input onfocus="focused(event)" onblur="unfocused(event)" type="tel" class="input"
+        placeholder="Telefonszám">
+</div>
+</div>
+<div class="input-area">
+<div class="input-title">
+    <p>Email</p>
+</div>
+<div class="input-block">
+    <input onfocus="focused(event)" onblur="unfocused(event)" id="email" type="email" class="input"
+        placeholder="Email">
+</div>
+</div>
+
+<div class="checkbox-row">
+<div class="box" onclick="getCheckbox()" >
+    <i class="fas fa-check checkIcon"></i>
+</div>
+<div class="text">
+    <p>Elfogadom az ÁSZF-et</p>
+</div>
+</div>
+`
+
+let page3 = ` <div style="display:flex;justify-content:center" >
+<lottie-player class="correct-animation" src="https://assets4.lottiefiles.com/packages/lf20_pmYw5P.json"
+    background="transparent" speed="0.5" autoplay>
+</lottie-player>
+
+</div>
+<p class="correct-feedback" >Sikeres regisztráció</p>`
+
+let buttons = `<div class="back-button" onclick="back()" >
+<span>
+    Vissza
+</span>
+</div>
+<div class="next-button" onclick="next()" >
+<span>
+    Tovább
+</span>
+</div>
+`
+
+let button = `<div class="next-button" onclick="next()" >
+<span>
+    Tovább
+</span>
+</div>`
+
+
+let activePage = 0;
+
+let pages = [page1, page2, page3];
+
+$(document).ready(() => {
+    $(".forms").append(page3)
+    indicator()
+    //$(".button-area").html(button);
+})
+$("#password2").ready(function () {
+    $("#password2").blur(function (e) {
+
+        if (accepted) {
+
+            let password = $("#password").val();
+            let password2 = $("#password2").val();
+            if (password != password2) {
+                alertBox("A jelszavak nem egyeznek")
+            }
+            else {
+                //Adatok begyűjtése és api hívás
+                $(".button-area").html(button);
+            }
+        }
+        else {
+            $(".button-area").html('');
+        }
+    });
+});
+
+function getCheckbox() {
+    let ok = checkbox();
+    if (!ok) {
+        $(".button-area").html('');
+    }
+    else {
+        $(".button-area").html(buttons);
+    }
+
+};
+
+
+function next() {
+    activePage++;
+    $(".forms").html(pages[activePage]);
+    if (activePage > 0 && activePage <= 1) {
+        $(".button-area").html('');
+    }
+    else if (activePage == 2) {
+        $(".button-area").html("");
+    }
+    else {
+        $(".button-area").html(button);
+    }
+indicator()
+};
+function back() {
+    activePage--;
+    $(".forms").html(pages[activePage]);
+    if (activePage > 0 && activePage <= 1) {
+        $(".button-area").html(buttons);
+    }
+    else {
+        $(".button-area").html(button);
+    }
+    indicator()
+};
+
+function indicator() {
+    for (let i = 0; i <= activePage; i++) {
+        $(".hr").eq(i).css("background-color", "#00c0bd")
+        $(".indicator-title").eq(i).css({
+            "border-color": "#00c0bd",
+            "color": "#00c0bd"
+        })
+    }
+
+}
--- script/main.js
+++ script/main.js
@@ -4,7 +4,16 @@ let password = new RegExp(/[a-zA-Z0-9]{8,28}/);
 let phone = new RegExp(/[0-9]{6,}/);
 let address = new RegExp(/[a-zA-Z0-9]{8,}/);
 let email = RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-let accepted = false
+
+let accepted = false;
+
+let inputData = {
+    name: "",
+    mobile: "",
+    email: "",
+    lakcim: ""
+}
+
 function focused(e) {
     let targetTitle = $(e.target).parent().parent().children(".input-title");
     let targetInput = $(e.target)
@@ -26,8 +35,14 @@ function unfocused(e) {
                     targetTitle.css("color", "red")
                     targetInput.css("border-color", "red")
                 }
+                else {
+                    inputData.name = $("#vezeteknev").val()+"_"+$("#keresztnev").val()
+                }
             }
-            else { accepted = true }
+            else {
+                accepted = true
+                inputData.lakcim = targetInput.val()
+            }
             break;
 
         case "password":
@@ -35,7 +50,7 @@ function unfocused(e) {
                 accepted = false;
                 targetTitle.css("color", "red")
                 targetInput.css("border-color", "red")
-                alert("A jelszónak legalább 8 karakternek kell lennie és csak betűt és számot tartalmazhat ")
+                alertBox("A jelszónak legalább 8 karakternek kell lennie és csak betűt és számot tartalmazhat ")
             } else { accepted = true }
             break;
         case "tel":
@@ -43,22 +58,30 @@ function unfocused(e) {
                 accepted = false;
                 targetTitle.css("color", "red")
                 targetInput.css("border-color", "red")
-            } else { accepted = true }
+            } else {
+                accepted = true
+                inputData.mobile = targetInput.val()
+            }
             break;
         case "email":
             if (!email.test($(targetInput).val())) {
                 accepted = false;
                 targetTitle.css("color", "red")
                 targetInput.css("border-color", "red")
-            } else { accepted = true }
+            } else {
+                accepted = true
+                inputData.email = targetInput.val()
+            }
             break;
         case "date":
-            console.log($(targetInput).val())
             if ($(targetInput).val() == null || $(targetInput).val() == "") {
                 accepted = false;
                 targetTitle.css("color", "red")
                 targetInput.css("border-color", "red")
-            } else { accepted = true }
+            } else {
+                accepted = true
+                inputData.date = targetInput.val()
+            }
             break;
     }
 }
@@ -67,9 +90,11 @@ function checkbox() {
     let icon = $(".checkIcon")
     if ($(icon).css("display") == "block") {
         icon.css("display", "none")
+       return  false;
     }
     else {
         icon.css("display", "block")
+       return true
     }
 
 }
@@ -78,16 +103,25 @@ function registration() {
     if (accepted) {
         let password = $("#password").val();
         let password2 = $("#password2").val();
-        if(password != password2){
-            alert("A jelszavak nem egyeznek")
+        if (password != password2) {
+            alertBox("A jelszavak nem egyeznek")
         }
-        else{
+        else {
             //Adatok begyűjtése és api hívás
         }
     }
     else {
-        alert("Kérjük ellenőrize az adatokat")
+        alertBox("Kérjük ellenőrize az adatokat")
     }
 }
 
+
+function alertBox(text) {
+    let alert = $(".alert-box");
+    $("#alert-text").text(text)
+    $(alert).css("display", "block")
+}
+function closeAlert() {
+    $(".alert-box").css("display", "none")
+}
 
